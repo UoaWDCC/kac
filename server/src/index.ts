25cb56c86351d549145d4717c93c219eade36945
Rayen Lee <rayenlee64@gmail.com>
2026-03-04 22:18:24 +1300

    feat: updated backend for testing

--- server/src/index.ts
+++ server/src/index.ts
@@ -4,10 +4,10 @@ const app: express.Application = express();
 
 const port: number = 3000;
 
-app.get('/', (req, res) => {
+app.get('/api', (req, res) => {
     res.send("Hi :)");
 });
 
 app.listen(port, () => {
-    console.log(`App is live on port ${port}`);
+    console.log(`Server application listening on port ${port}`);
 });

d79a51ae1178d9901b953d97f9791c4ca5aaded3
Rayen Lee <rayenlee64@gmail.com>
2026-03-05 15:40:19 +1300

    mongodb set-up

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,13 +1,38 @@
-import express from 'express';
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import { Testing } from "./model/testing";
 
-const app: express.Application = express();
+dotenv.config({ quiet: true });
 
 const port: number = 3000;
+const app: express.Application = express();
+const mongoUrl: string = `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@kac-prod.cf1fyh5.mongodb.net/`;
 
-app.get('/api', (req, res) => {
-    res.send("Hi :)");
+app.get("/api/test", (req, res) => {
+  res.send("Hi :)");
 });
 
-app.listen(port, () => {
-    console.log(`Server application listening on port ${port}`);
+app.post("/api/test", (req, res) => {
+  const testing = { name: "Default Name", value: 0 };
+  Testing.create(testing)
+    .then(() => {
+      res.send("Test successful!");
+    })
+    .catch((err) => {
+      console.error("Error creating testing document:", err);
+      res.status(500).send("Error creating testing document");
+    });
 });
+
+mongoose
+  .connect(mongoUrl)
+  .then(() => {
+    console.log("Connected to MongoDB");
+    app.listen(port, () => {
+      console.log(`Server application listening on port ${port}`);
+    });
+  })
+  .catch((err) => {
+    console.error("Error connecting to MongoDB:", err);
+  });

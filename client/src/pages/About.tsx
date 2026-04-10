4f0469e5f104497de79601f0c45477e5118a2769
anqilique <angelbhulot@gmail.com>
2026-04-10 19:47:57 +1200

    feat: add title + exec group image

--- client/src/pages/About.tsx
+++ client/src/pages/About.tsx
@@ -1,5 +1,33 @@
+import "../style/common.css";
+import exec_team from "../images/exec_team.png";
+
 const About = () => {
-  return <div>About page - not yet implemented</div>;
+  return <div>
+    {/** TITLE */}
+      <section
+        className="section yellow-bg"
+        style={{
+          width: "100%",
+          textAlign: "center",
+        }}
+      >
+        <h1 
+          style={{
+            fontWeight: 700, fontSize: 46,  // styling is rough. just eyeballed it.
+          }}>
+          A B O U T &nbsp; U S
+        </h1>
+        <img
+          src={ exec_team }  // a screenshot placeholder, no access to official file?
+          alt="Kiwi Asian Club Executive Team"
+          style={{
+            maxWidth: "960px",
+            width: "100%",
+            height: "auto",
+          }}
+        ></img>
+      </section>
+  </div>;
 };
 
 export default About;

e27c8db4bbc0b4a89af759fae7d667e4bd74162e
Will Jin <falidapanda@gmail.com>
2026-04-12 20:41:43 +1200

    feat(faq-page): added title with styling similar to old website

--- client/src/pages/Faq.tsx
+++ client/src/pages/Faq.tsx
@@ -1,5 +1,18 @@
 const Faq = () => {
-  return <div>Faq page - not yet implemented</div>;
+  return (
+  <div>
+    {/** title */}
+    <section
+      className="section yellow-bg"
+      style={{
+        width: "100%",
+        textAlign: "center",
+      }}
+    >
+      <h1>F &nbsp; A &nbsp; Q &nbsp; S</h1>
+    </section>
+  </div>
+  );
 };
 
 export default Faq;

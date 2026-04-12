34b888ce457003deddd7f6e8bbbf7481207e7b89
Will Jin <falidapanda@gmail.com>
2026-04-12 21:17:58 +1200

    feat(faq-page): moved styling into css file and updated tsx to use css appropriately

--- client/src/pages/Faq.tsx
+++ client/src/pages/Faq.tsx
@@ -1,15 +1,23 @@
+import React from "react";
+import "../style/faq.css";
+
 const Faq = () => {
   return (
-  <div>
+  <div className="faq-container">
     {/** title */}
     <section
-      className="section yellow-bg"
-      style={{
-        width: "100%",
-        textAlign: "center",
-      }}
+      className="faq-title"
     >
       <h1>F &nbsp; A &nbsp; Q &nbsp; S</h1>
+    </section>
+
+    {/** faq items */}
+    <section
+    className = "faq-item"
+    style={{
+      width: "100%"
+    }}>
+
     </section>
   </div>
   );

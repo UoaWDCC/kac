1c4089e5e3eb526b04db7a87ec58f6631c9d17e4
anqilique <angelbhulot@gmail.com>
2026-04-13 19:42:28 +1200

    feat: separate styling from .tsx file into own .css file

--- client/src/pages/About.tsx
+++ client/src/pages/About.tsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { ChevronDown, ChevronUp } from 'lucide-react';
 
 import "../style/common.css";
+import "../style/about.css";
 import exec_team from "../images/exec_team.png";
 
 /** 
@@ -17,25 +18,11 @@ const Collapsible: React.FC<CollapsibleProps> = ({ title, children }) => {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <div className="">
+    <div>
       {/* Click to toggle the collapsible */}
-      <button onClick={() => setIsOpen(!isOpen)}
-        style={{
-          width: "100%",
-          background: "none",
-          border: "none"
-        }}
-      >
-        <div style={{
-          display: "flex",
-          justifyContent: "space-between",
-          flexWrap: "wrap",
-          alignItems: "center",
-          fontSize: 22,
-          borderBottom: "1px solid",
-        }}
-        >
-          <h2>{title}</h2>
+      <button onClick={ () => setIsOpen(!isOpen) }>
+        <div className="collapsible-content">
+          <h2 className="collapsible-title">{title}</h2>
           <span>
             {isOpen ? <ChevronUp className="dynamic-size-icon" /> : <ChevronDown className="dynamic-size-icon" />}
           </span>
@@ -44,9 +31,7 @@ const Collapsible: React.FC<CollapsibleProps> = ({ title, children }) => {
 
       {/* Render when opening the section */}
       {isOpen && (
-        <div
-          style={{textAlign: "left"}}
-        >
+        <div className="collapsible-children">
           {children}
         </div>
       )}
@@ -63,18 +48,8 @@ const About = () => {
   return <div>
     <title>Kiwi Asian Club - About Us</title>
     {/** Overview */}
-    <section
-      className="section yellow-bg"
-      style={{
-        width: "100%",
-        textAlign: "center",
-      }}
-    >
-      <h1
-        style={{
-          fontWeight: 700, fontSize: 46,
-        }}
-      >
+    <section className="section yellow-bg title">
+      <h1 className="title-text">
         A B O U T &nbsp; U S
       </h1>
       <img
@@ -86,20 +61,21 @@ const About = () => {
       {/** What We Do */}
       <div className="medium-content">
         <Collapsible title="WHAT WE DO">
-          <p
-            style={{ fontSize: 26 }}
-          >
+          <p className="header-body">
             The Kiwi Asian Club is a group for everyone on and off campus!
             We have been one of the most active clubs on campus since 2001, hosting weekly social,
             cultural, sporting, and charity events throughout the university year.
-            <br/><br/>
+          </p>
+          <p className="header-body">
             We aim to build lasting friendships and help you form meaningful connections and memories.
             With over 1000 members, we celebrate the cultural diversity of students in New Zealand,
             and people of all backgrounds are welcome to join.
-            <br/><br/>
+          </p>
+          <p className="header-body">
             If you're a new member, don't worry! We hold team-based events for people to meet each other,
             and our friendly executive team is here to help you along the way.
-            <br/><br/>
+          </p>
+          <p className="header-body">
             Keep yourself informed by visiting our LinkTree to sign up for events
             and connect with us on social media!
           </p>

818bc7e5e06b1ea8688d48ec33c361dbb53cba59
Vincent Tao <vincent.jh.tao@gmail.com>
2026-04-16 15:38:42 +1200

    feat: implement login/logout functionality on fe header

--- client/src/main/Header.tsx
+++ client/src/main/Header.tsx
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom";
+import { useAuth } from "../auth/useAuth.ts";
 
 const linkStyle = {
   textDecoration: "none",
@@ -12,6 +13,8 @@ const headerStyle = {
 };
 
 const Header = () => {
+  const { user, loading, logout } = useAuth();
+
   return (
     <header className="yellow-bg" style={headerStyle}>
       <Link to="/" style={linkStyle}>
@@ -36,6 +39,26 @@ const Header = () => {
         <Link to="/Faq" style={linkStyle}>
           Faq
         </Link>
+
+        {!loading &&
+          (user ? (
+            <div>
+              <img src={user.photos[0].value} alt="profile" />
+              <span>{user.displayName}</span>
+              <button
+                onClick={logout}
+                style={{
+                  ...linkStyle,
+                }}
+              >
+                Sign Out
+              </button>
+            </div>
+          ) : (
+            <a href="/auth/google" style={linkStyle}>
+              Sign In
+            </a>
+          ))}
       </div>
     </header>
   );

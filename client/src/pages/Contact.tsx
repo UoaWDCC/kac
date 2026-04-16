6d05ee3107e88b8f44ad4e868d3032043d87ead6
pxstachio <james@swaz.nz>
2026-04-16 23:03:51 +1200

    removed local storage and submit state

--- client/src/pages/Contact.tsx
+++ client/src/pages/Contact.tsx
@@ -1,15 +1,13 @@
 import { useState } from "react";
+import "../style/common.css";
 import "../style/contact.css";
 
 const Contact = () => {
-
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [message, setMessage] = useState("");
 
-  const [submitted, setSubmitted] = useState(false);
-
-  const handleSubmit = (e: React.FormEvent) => {
+  const handleSubmit = (e: React.SubmitEvent<HTMLFormElement>) => {
     e.preventDefault();
 
     const contactData = {
@@ -18,16 +16,7 @@ const Contact = () => {
       message,
     };
 
-    const existing = localStorage.getItem("contactus")
-    const contacts = existing ? JSON.parse(existing) : [];
-
-    contacts.push(contactData);
-
-    localStorage.setItem("contactus", JSON.stringify(contacts));
-
     console.log("Saved: ", contactData);
-
-    setSubmitted(true)
 
     setName("");
     setEmail("");
@@ -35,46 +24,44 @@ const Contact = () => {
   };
 
   return (
-    <div className = "contact-page">
-      <h1 className = "contact-title">CONTACT US</h1>
-    
-      <p className = "contact-intro">
+    <div className="contact-page">
+      <h1 className="contact-title">CONTACT US</h1>
+
+      <p className="contact-intro">
         Sign up using the form below or let us know any questions you have!
       </p>
 
-      {submitted ? (
-        <h2 className = "contact-submitted">Submitted!</h2>
-      ) : (
-        <form className="contact-form" onSubmit={handleSubmit}>
-          <div>
-            <input
-              type = "text"
-              value = {name}
-              placeholder = "Enter your name"
-              onChange = {(e) => setName(e.target.value)}
-            />
-          </div>
+      <form className="contact-form" onSubmit={handleSubmit}>
+        <div>
+          <input
+            type="text"
+            value={name}
+            placeholder="Enter your name"
+            onChange={(e) => setName(e.target.value)}
+          />
+        </div>
 
-          <div>
-            <input
-              type = "text"
-              value = {email}
-              placeholder = "Enter your email"
-              onChange = {(e) => setEmail(e.target.value)}
-            />
-          </div>
+        <div>
+          <input
+            type="text"
+            value={email}
+            placeholder="Enter your email"
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
 
-          <div>
-            <textarea
-              value = {message}
-              placeholder = "Write your message"
-              onChange = {(e) => setMessage(e.target.value)}
-            />
-          </div>
+        <div>
+          <textarea
+            value={message}
+            placeholder="Write your message"
+            onChange={(e) => setMessage(e.target.value)}
+          />
+        </div>
 
-          <button className = "contact-button" type = "submit">Send</button>
-        </form>
-      )}
+        <button className="contact-button" type="submit">
+          Send
+        </button>
+      </form>
     </div>
   );
 };

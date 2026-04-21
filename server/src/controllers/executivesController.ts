fc4cc3682539959dd6b4ccd9bd5958949371193e
anqilique <angelbhulot@gmail.com>
2026-04-21 13:46:52 +1200

    feat: create schema, controller to be filled in

---
+++ server/src/controllers/executivesController.ts
@@ -0,0 +1,10 @@
+import { Executive } from "../model/executive";
+import { RequestHandler } from "express";
+
+export const addExec: RequestHandler = async (req, res, next) => {};
+
+export const editExec: RequestHandler = async (req, res, next) => {};
+
+export const deleteExec: RequestHandler = async (req, res, next) => {};
+
+export const getAllExecs: RequestHandler = async (req, res, next) => {};

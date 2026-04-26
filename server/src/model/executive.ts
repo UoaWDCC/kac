ff501493e1dca2fa4bc211c5a845158c67792b58
anqilique <angelbhulot@gmail.com>
2026-04-26 23:21:55 +1200

    feat: complete controller, middleware, routes, & adjust schema

--- server/src/model/executive.ts
+++ server/src/model/executive.ts
@@ -9,14 +9,14 @@ const executiveSchema = new Schema(
     role: { type: String, required: true },
     description: { type: String, required: true },
     fullName: { type: String, required: true },
-    ethnicity: { type: String, required: true },
-    degree: { type: String, required: true },
+    ethnicity: { type: String, required: false },
+    degree: { type: String, required: false },
     mbti: { type: String, required: true },
     fact: { type: String, required: true },
     sponsor: { type: String, required: true },
     greenFlag: { type: String, required: true },
     redFlag: { type: String, required: true },
-    emojis: { type: String, required: true },
+    emojis: { type: String, required: false },
   },
   { timestamps: true }
 );

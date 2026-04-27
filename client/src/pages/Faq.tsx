d7094ac992b12bd5ecf2afeadb5f105ad0e450d4
Will Jin <falidapanda@gmail.com>
2026-04-28 02:18:05 +1200

    feat(faq-backend_: updated faq to fetch faq data from mongodb instead of placeholder

--- client/src/pages/Faq.tsx
+++ client/src/pages/Faq.tsx
@@ -1,15 +1,28 @@
+import React, { useState, useEffect } from "react";
 import Collapsible from "../components/Collapsible";
 
 import "../style/faq.css";
 import "../style/common.css";
-import faqs from "../placeholders/faqs.json";
+
+interface Faq {
+  question: string;
+  answer: string;
+}
 
 const Faq = () => {
+  const [faqs, setFaqs] = useState<Faq[]>([]);
+
+  useEffect(() => {
+    fetch("/api/faqs")
+    .then((res) => res.json())
+    .then((data) => setFaqs(data))
+    .catch((err) => console.error("Failed to fetch faqs:", err));
+  }, [])
+
   return (
     <div className="faq-container yellow-bg">
       {/** title */}
       <section className="faq-title">F&nbsp;A&nbsp;Q&nbsp;s</section>
-
       {/** faq items */}
       <section className="faq-list">
         {faqs.map((item, index) => (

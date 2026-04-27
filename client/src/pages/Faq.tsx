199ceec6833a268764a1cd5565602561e4ed4bf8
Will Jin <falidapanda@gmail.com>
2026-04-28 01:25:39 +1200

    chore(faq-page): ran prettier on 3 files that failed codestyle

--- client/src/pages/Faq.tsx
+++ client/src/pages/Faq.tsx
@@ -1,31 +1,24 @@
 import Collapsible from "../components/Collapsible";
 
 import "../style/faq.css";
-import "../style/common.css"
+import "../style/common.css";
 import faqs from "../placeholders/faqs.json";
 
 const Faq = () => {
   return (
-  <div className="faq-container yellow-bg">
-    {/** title */}
-    <section
-      className="faq-title"
-    >
-      F&nbsp;A&nbsp;Q&nbsp;s
-    </section>
+    <div className="faq-container yellow-bg">
+      {/** title */}
+      <section className="faq-title">F&nbsp;A&nbsp;Q&nbsp;s</section>
 
-    {/** faq items */}
-    <section
-    className = "faq-list"
-    >
-      {faqs.map((item, index) => (
-        <Collapsible key={index} title={item.question}>
-          {item.answer}
-        </Collapsible>
-      ))}
-
-    </section>
-  </div>
+      {/** faq items */}
+      <section className="faq-list">
+        {faqs.map((item, index) => (
+          <Collapsible key={index} title={item.question}>
+            {item.answer}
+          </Collapsible>
+        ))}
+      </section>
+    </div>
   );
 };
 

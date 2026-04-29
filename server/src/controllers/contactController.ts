41ac6de63a331163fc53e6c53ec16e6084fecb68
pxstachio <james@swaz.nz>
2026-04-30 00:36:42 +1200

    Created schema, controller to fill

---
+++ server/src/controllers/contactController.ts
@@ -0,0 +1,6 @@
+import { Contact } from "../model/contact";
+import { RequestHandler } from "express";
+
+export const addContact: RequestHandler = async (req, res) => {};
+
+export const getContacts: RequestHandler = async (req, res) => {};

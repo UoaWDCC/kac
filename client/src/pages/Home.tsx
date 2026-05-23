9649687b00d3bfadbff0c596e89ca6d0d08df8b4
anqilique <angelbhulot@gmail.com>
2026-05-23 16:30:18 +1200

    style: polish What We Do section

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -25,7 +25,7 @@ const Home = () => {
       {/** WHAT WE DO */}
       <section className="section bg-yellow-light h-200 flex flex-col items-center justify-center gap-12">
         <div
-          className="bg-yellow-dark w-8/10 justify-self-center rounded-lg h-auto py-12 px-8"
+          className="bg-yellow-dark w-9/10 2xl:w-8/10 justify-self-center rounded-lg h-auto py-12 px-8"
           style={{ boxShadow: "10px 10px var(--color-yellow-medium)" }}
         >
           <div className="flex items-start gap-8">
@@ -42,26 +42,28 @@ const Home = () => {
               />
             </div>
             <div className="w-4/10 flex flex-col justify-between h-full self-center">
-              <h1 className="mt-0! mb-0! font-monospace">WHAT WE DO</h1>
-              <p className="py-1">
+              <h2 className="mt-0! mb-0! font-monospace text-[2.6rem] font-medium">
+                WHAT WE DO:
+              </h2>
+              <p className="py-1 font-alan-sans">
                 The Kiwi Asian Club is a group for everyone on and off campus!
                 We have been one of the most active clubs on campus since 2001,
                 hosting weekly social, cultural, sporting, and charity events
                 throughout the university year.
               </p>
-              <p className="py-1">
+              <p className="py-1 font-alan-sans">
                 We aim to build lasting friendships and help you form meaningful
                 connections and memories.
               </p>
               <div className="hidden 2xl:block">
-                <p className="py-1">
+                <p className="py-1 font-alan-sans">
                   With over 1000 members, we celebrate the cultural diversity of
                   students in New Zealand, and people of all backgrounds are
                   welcome to join. If you're a new member, don't worry! We hold
                   team-based events for people to meet each other, and our
                   friendly executive team is here to help you along the way.
                 </p>
-                <p className="py-1">
+                <p className="py-1 font-alan-sans">
                   Keep yourself informed by visiting our LinkTree to sign up for
                   events and connect with us on social media!
                 </p>
@@ -70,6 +72,7 @@ const Home = () => {
                 <Link
                   to="/about"
                   className="px-8 py-2 rounded-full relative text-decoration-none text-yellow-light bg-blue-medium w-0.8 hover:bg-blue-light duration-300"
+                  style={{ boxShadow: "1px 4px var(--color-grey-medium)" }}
                 >
                   Learn More {">>"}
                 </Link>

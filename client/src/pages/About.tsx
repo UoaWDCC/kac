2117163f2c03eeb61db13312086948027865b1e6
Will Jin <falidapanda@gmail.com>
2026-05-25 19:01:47 +1200

    feat(about-frontend): added kac logo to title

--- client/src/pages/About.tsx
+++ client/src/pages/About.tsx
@@ -1,4 +1,5 @@
 import Executives from "../components/Executives.tsx";
+import kacoTitle from "../images/kaco-title.png";
 
 import "../style/common.css";
 import "../style/about.css";
@@ -12,8 +13,11 @@ const About = () => {
     <div className="about-page">
       <title>Kiwi Asian Club - About Us</title>
       {/** Title **/}
-      <section className="section h1">
-        <h1 className="title-text font-sans">MEET &nbsp;THE &nbsp;EXECS</h1>
+      <section className="section h1 about-title-section">
+        <div className="about-title-group">
+          <img className="about-title-kaco" src={kacoTitle} alt="Kaco mascot" />
+          <h1 className="title-text font-sans">MEET &nbsp;THE &nbsp;EXECS</h1>
+        </div>
       </section>
 
       {/** Executive Cards */}

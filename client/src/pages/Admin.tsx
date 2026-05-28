c76b051d0630f09b14c646eb11e37d61224e2cad
Conrad <cang688@aucklanduni.ac.nz>
2026-05-28 16:42:46 +1200

    fixed dependency issues and added the admin guard for the admin page

--- client/src/pages/Admin.tsx
+++ client/src/pages/Admin.tsx
@@ -1,13 +1,16 @@
 import "../style/common.css";
 import AdminSidebar from "../components/AdminComponents/AdminSidebar.tsx";
 import AdminDashboard from "../components/AdminComponents/AdminDashboard.tsx";
+import { AdminRoute } from "../auth/AdminRoute.tsx";
 
 const Admin = () => {
     return (
-        <div className="grid grid-cols-[1fr_3fr] gap-2 h-screen margin-4">
-            <AdminSidebar />
-            <AdminDashboard />
-        </div>
+        <AdminRoute>
+            <div className="grid grid-cols-[1fr_3fr] gap-2 h-screen margin-4">
+                <AdminSidebar />
+                <AdminDashboard />
+            </div>
+        </AdminRoute>
     )
 }
 

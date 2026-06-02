8e75f2e5080362cb319d398ec93b01fbcef42079
vincenttao04 <vincent.jh.tao@gmail.com>
2026-06-02 23:14:48 +1200

    fix: replace membershipqr with pass2u link

--- client/src/pages/Profile.tsx
+++ client/src/pages/Profile.tsx
@@ -1,11 +1,15 @@
-import MembershipQR from "../components/MembershipQR";
+// TODO: replace with dynamic link fetched from admin dashboard once implemented
+const PASS2U_LINK =
+  "https://www.pass2u.net/p/mZvjfJH6V37n?openExternalBrowser=1";
 
 const Profile = () => {
   return (
-    <>
-      <p>Profile</p>
-      <MembershipQR />
-    </>
+    <div>
+      <h1>My Membership</h1>
+      <a href={PASS2U_LINK} target="_blank" rel="noopener noreferrer">
+        Add to Wallet
+      </a>
+    </div>
   );
 };
 

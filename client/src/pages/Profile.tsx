1ce72e94a2dc76d02caee3f0c04cfcde717240a4
Vincent Tao <vincent.jh.tao@gmail.com>
2026-07-29 00:50:16 +1200

    feat: new profile page, consistent header styles applied

--- client/src/pages/Profile.tsx
+++ client/src/pages/Profile.tsx
@@ -1,14 +1,24 @@
+import kacoTitle from "../images/kaco-title.png";
+
+import "../style/common.css";
+import "../style/profile.css";
+
 // TODO: replace with dynamic link fetched from admin dashboard once implemented
 const PASS2U_LINK =
   "https://www.pass2u.net/p/mZvjfJH6V37n?openExternalBrowser=1";
 
 const Profile = () => {
   return (
-    <div>
-      <h1>My Membership</h1>
-      <a href={PASS2U_LINK} target="_blank" rel="noopener noreferrer">
-        Add to Wallet
-      </a>
+    <div className="profile-page">
+      <title>Kiwi Asian Club - My Profile</title>
+
+      {/** Title **/}
+      <section className="section h1 profile-title-section">
+        <div className="profile-title-group">
+          <img className="about-title-kaco" src={kacoTitle} alt="Kaco mascot" />
+          <h1 className="title-text font-sans">MY &nbsp;PROFILE</h1>
+        </div>
+      </section>
     </div>
   );
 };

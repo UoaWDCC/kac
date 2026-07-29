314475b9034c3e91585edc9ec5a9a7eaccca7499
Rayen Lee <rayenlee64@gmail.com>
2026-07-29 16:22:10 +1200

    fix: nginx 404 error
    also needed to make some backend modifications so changes worked with google auth

--- server/src/index.ts
+++ server/src/index.ts
@@ -101,7 +101,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID!,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
-      callbackURL: "/api/auth/google/callback",
+      callbackURL: `${process.env.SERVER_URL}/api/auth/google/callback`,
     },
     async (_accessToken, _refreshToken, profile, done) => {
       try {

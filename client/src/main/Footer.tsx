38a3d288990c5ce36f409529f2cddfc5520a5867
pxstachio <james@swaz.nz>
2026-08-10 23:23:21 +1200

    feat: added footer

--- client/src/main/Footer.tsx
+++ client/src/main/Footer.tsx
@@ -1,5 +1,65 @@
+import "../style/common.css";
+import "../style/footer.css";
+
 const Footer = () => {
-  return <footer></footer>;
+  return (
+    <footer className="footer">
+      <div className="footer-column">
+        <a href="/about">
+          <h3>About</h3>
+        </a>
+
+        <a href="/about">Our History</a>
+        <a href="/about">Our Execs</a>
+      </div>
+
+      <div className="footer-column">
+        <a href="/events">
+          <h3>Events</h3>
+        </a>
+
+        <a href="/events">Upcoming Events</a>
+        <a href="/events">Past Events</a>
+      </div>
+
+      <div className="footer-column">
+        <a href="/sponsors">
+          <h3>Sponsors</h3>
+        </a>
+
+        <a href="/sponsors">CBD Sponsors</a>
+        <a href="/sponsors">New Market Sponsors</a>
+        <a href="/sponsors">Other Sponsors</a>
+      </div>
+
+      <div className="footer-column">
+        <a href="/contact">
+          <h3>Contact Us</h3>
+        </a>
+
+        <a href="/contact">Contact</a>
+        <a href="/contact">FAQ's</a>
+      </div>
+
+      <div className="footer-column">
+        <h3>Connect With Us!</h3>
+
+        <div>
+          <a href="https://www.instagram.com/kiwiasianclub/?hl=en">Instagram</a>
+          <a href="https://www.facebook.com/kiwiasianclub">Facebook</a>
+          <a href="https://www.tiktok.com/@kiwiasianclub?_r=1&_t=ZS-92szPPEDCNj">
+            Tiktok
+          </a>
+          <a href="mailto:kiwiasian@gmail.com">Email</a>
+        </div>
+      </div>
+
+      <div className="footer-bottom">
+        <img src="src/images/kaco-title.png" alt="Club logo" />
+        <p>© 2026 Kiwi Asian Club</p>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;

0069b97b3ae4da82929faa7660c884407d8fe36a
Vincent Tao <vincent.jh.tao@gmail.com>
2026-08-14 16:49:11 +1200

    refactor: disable event schema versioning

--- server/src/model/event.ts
+++ server/src/model/event.ts
@@ -2,12 +2,15 @@ import mongoose from "mongoose";
 
 const { Schema, model } = mongoose;
 
-const eventSchema = new Schema({
-  title: { type: String, required: true },
-  description: { type: String, required: true },
-  imageUrl: { type: String, required: true },
-  datetime: { type: Date, required: true },
-  capacity: { type: Number, required: false },
-});
+const eventSchema = new Schema(
+  {
+    title: { type: String, required: true },
+    description: { type: String, required: true },
+    imageUrl: { type: String, required: true },
+    datetime: { type: Date, required: true },
+    capacity: { type: Number, required: false },
+  },
+  { versionKey: false }
+);
 
 export const Event = model("Event", eventSchema);

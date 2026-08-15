dcf4d6ab4207adc894fdf5e980e80d92828cb84a
anqilique <angelbhulot@gmail.com>
2026-08-15 21:40:39 +1200

    style: NewExecModal style updated

--- client/src/components/NewExecModal.tsx
+++ client/src/components/NewExecModal.tsx
@@ -9,7 +9,7 @@ interface ModalProps {}
 export default function Modal({}: ModalProps) {
   const [isOpen, setIsOpen] = useState<boolean>(false);
 
-  // const openModal = (): void => setIsOpen(true);
+  const openModal = (): void => setIsOpen(true);
   const closeModal = (): void => setIsOpen(false);
 
   const handleOverlayClick = (e: React.MouseEvent<HTMLDivElement>): void => {
@@ -20,40 +20,57 @@ export default function Modal({}: ModalProps) {
 
   return (
     <div className="add-exec-container">
-      <button className="add-exec-button">
+      <button className="add-exec-button" type="button" onClick={openModal}>
         <Plus className="dynamic-size-icon" />
         <p>New Executive</p>
       </button>
 
       {isOpen && (
-        <div className="modal-overlay" onClick={handleOverlayClick}>
-          <div className="modal-content">
-            <h2>New Executive</h2>
-            <form className="exec-form">
-              <div>
-                <label>Display Name</label>
-                <input type="text" placeholder="e.g., John" />
+        <div
+          className="fixed inset-0 background-blur flex items-center justify-center z-1000"
+          onClick={handleOverlayClick}
+        >
+          <div className="font-alan-sans bg-white rounded-2xl px-16 py-10 justify-center relative border-box min-w-[36vw] h-fit flex flex-col gap-6">
+            <h2 className="text-2xl! font-bold">New Executive</h2>
+            <form className="flex flex-col gap-8">
+              <div className="flex flex-col gap-1">
+                <h3 className="text-lg font-bold">Name</h3>
+                <input
+                  type="text"
+                  placeholder="Kaco"
+                  className="w-full py-2 border-t-0 border-l-0 border-r-0 border-b-yellow-dark border-2 outline-none"
+                />
               </div>
 
-              <div>
-                <label>Executive Role</label>
-                <input type="text" placeholder="e.g., President" />
+              <div className="flex flex-col gap-1">
+                <h3 className="text-lg font-bold">Role</h3>
+                <input
+                  type="text"
+                  placeholder="Mascot"
+                  className="w-full py-2 border-t-0 border-l-0 border-r-0 border-b-yellow-dark border-2 outline-none"
+                />
               </div>
 
-              <div>
-                <label>Full Name</label>
-                <input type="text" placeholder="e.g., John Doe" />
+              <div className="flex flex-col gap-1">
+                <h3 className="text-lg font-bold">Description</h3>
+                <textarea
+                  cols={50}
+                  rows={2}
+                  wrap="hard"
+                  placeholder="Kaco is the mascot of the Kiwi Asian Club..."
+                  className="resize-none w-full py-2 border-t-0 border-l-0 border-r-0 border-b-yellow-dark border-2 outline-none"
+                ></textarea>
               </div>
-
-              <div>
-                <label>Description</label>
-                <textarea placeholder="Brief description of the executive" />
-              </div>
-
-              <button className="contact-button" type="submit">
-                Create Executive
-              </button>
             </form>
+            <div className="flex justify-center mt-4">
+              <button
+                className="cursor-pointer py-2 px-12 w-fit! h-10 rounded-3xl text-blue-medium hover:text-yellow-light bg-yellow-dark! hover:bg-blue-medium! duration-200 shadow-[2px_4px] shadow-yellow-medium hover:shadow-gray-400"
+                type="submit"
+                onClick={closeModal}
+              >
+                Create!
+              </button>
+            </div>
           </div>
         </div>
       )}

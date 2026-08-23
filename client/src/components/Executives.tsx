5457d7191c04314c8bbacc793741c0fac91bc37b
anqilique <angelbhulot@gmail.com>
2026-08-23 19:46:07 +1200

    style: adjust spacing and text sizes for exec modals

--- client/src/components/Executives.tsx
+++ client/src/components/Executives.tsx
@@ -192,16 +192,16 @@ const Executives = () => {
               <X size={40} strokeWidth={2} />
             </div>
 
-            <div className="flex flex-row gap-4 text-blue-medium items-center w-full p-12">
+            <div className="flex flex-row gap-8 text-blue-medium items-center w-full p-12">
               <img
                 className="flex rounded-4xl h-full"
                 src={selectedExec.imageURL || EXEC_IMG}
                 alt={selectedExec.displayName}
               />
 
-              <div className="flex flex-col gap-4 2xl:gap-10 px-4 py-2 h-full justify-end 2xl:justify-center">
+              <div className="flex flex-col gap-8 2xl:gap-10 px-4 py-2 h-full justify-center">
                 <div className="flex flex-col gap-1">
-                  <h4 className="font-monospace font-semibold text-[20px] uppercase pl-1">
+                  <h4 className="font-monospace font-semibold text-[20px] uppercase">
                     {selectedExec.execRole}
                   </h4>
                   <h3 className="text-4xl uppercase">
@@ -210,32 +210,32 @@ const Executives = () => {
                 </div>
                 <div className="flex flex-col gap-2 2xl:gap-8 font-alan-sans">
                   <div>
-                    <p className="text-lg! 2xl:text-xl!">
+                    <p className="text-md! 2xl:text-xl!">
                       <strong>Ethnicity:</strong> {selectedExec.ethnicity}
                     </p>
-                    <p className="text-lg! 2xl:text-xl!">
+                    <p className="text-md! 2xl:text-xl!">
                       <strong>Degree:</strong> {selectedExec.degree}
                     </p>
-                    <p className="text-lg! 2xl:text-xl!">
+                    <p className="text-md! 2xl:text-xl!">
                       <strong>MBTI:</strong> {selectedExec.mbti}
                     </p>
                   </div>
 
                   <div>
-                    <p className="text-lg! 2xl:text-xl!">
+                    <p className="text-md! 2xl:text-xl!">
                       <strong>Fun Fact:</strong> {selectedExec.fact}
                     </p>
-                    <p className="text-lg! 2xl:text-xl!">
+                    <p className="text-md! 2xl:text-xl!">
                       <strong>Favourite KAC Sponsor:</strong>{" "}
                       {selectedExec.sponsor}
                     </p>
-                    <p className="text-lg! 2xl:text-xl!">
+                    <p className="text-md! 2xl:text-xl!">
                       <strong>Green Flag:</strong> {selectedExec.greenFlag}
                     </p>
-                    <p className="text-lg! 2xl:text-xl!">
+                    <p className="text-md! 2xl:text-xl!">
                       <strong>Red Flag:</strong> {selectedExec.redFlag}
                     </p>
-                    <p className="text-lg! 2xl:text-xl!">
+                    <p className="text-md! 2xl:text-xl!">
                       <strong>Fav Emojis:</strong> {selectedExec.emojis}
                     </p>
                   </div>

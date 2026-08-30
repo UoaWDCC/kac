4bff3526f30ef7a5cb71642ca093e0ee8bd28912
cang688 <cang688@aucklanduni.ac.nz>
2026-08-31 11:03:40 +1200

    updated the image with image block

--- client/src/components/PageTitle.tsx
+++ client/src/components/PageTitle.tsx
@@ -1,11 +1,13 @@
+import { ImageBlock } from "./image_block/ImageBlock";
+
 const PageTitle = ({ title }: { title: string }) => {
   return (
     <div style={{ maxWidth: "1200px", margin: "0 auto", padding: "0 2rem" }}>
       <section style={{ padding: "6rem 0 2rem 0" }}>
         <div style={{ position: "relative", display: "inline-block" }}>
-          <img
-            src="src/images/kaco-title.png"
-            alt="Mascot"
+          <ImageBlock
+            pageKey="title"
+            alt="Club Mascot"
             style={{
               position: "absolute",
               left: "-95px",
@@ -13,6 +15,7 @@ const PageTitle = ({ title }: { title: string }) => {
               width: "200px",
               zIndex: 2,
             }}
+            editable={false}
           />
           <h1
             className="sponsors-title"
